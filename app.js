f9248800638aeebc95a4a942b46ead8bab84050d
Javi <javier.s396756@cesurformacion.com>
2026-06-24 20:54:41 +0200

    Conexion bd

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ var mysql = require('mysql');
 
 
 var app = express();
+// Configuración de la conexión a la base de datos MySQL
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'articulosbd'
+});
+// Conexión a la base de datos
+connection.connect(function (error) {
+    if (error) {
+        throw error;
+    }else {
+        console.log('Conexión exitosa a la base de datos MySQL :D');
+    }
+});
 
 app.get('/', function (req, res) {
     res.send('Ruta de inicio del servidor');
