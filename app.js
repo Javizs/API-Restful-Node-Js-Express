fb5d50cdd5fd6c2ffc047fdd8611108a50640cb4
Javi <javier.s396756@cesurformacion.com>
2026-06-24 22:34:37 +0200

    Añadiendo el resto de metodos de la api

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var mysql = require('mysql');
 
 
 var app = express();
+
+//Especifiacion de que usamos JSON
+app.use(express.json());
+
 // Configuración de la conexión a la base de datos MySQL
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -35,14 +39,82 @@ connection.connect(function (error) {
         if (error) {
             throw error; 
         }else{
-            //res.send(fila[0]);
-            res.send(fila[0].descripcion);
+            res.send(fila);
+            //res.send(fila[0].descripcion);
         }
     });
 });
 app.get('/', function (req, res) {
     res.send('Ruta de inicio del servidor');
 });
+
+
+app.post('/api/articulos', (req, res) => {
+
+    let data = {descripcion:req.body.descripcion,precio:req.body.precio,stock:req.body.stock};
+    let sql = "Insert into articulos SET ?";
+    connection.query(sql,data,function (error, results) {
+        if (error) {
+            throw error; 
+        }else{
+            res.send(results);
+      
+        }
+    });
+});
+
+//METODO EDITAR (UPDATE) ARTICULO PUT
+
+app.put('/api/articulos/:id', (req, res) => {
+    let id = req.params.id;
+    let descripcion = req.body.descripcion;
+    let precio = req.body.precio;
+    let stock = req.body.stock;
+    let sql = "UPDATE articulos set descrpcion =  ?, precio = ?, stock = ? WHERE id = ?";
+    connection.query(sql, [descripcion, precio, stock, id], function (error, results) {
+        if (error) {
+            throw error; 
+        }else{
+            res.send(results);
+        }
+    });
+
+});
+
+//METODO ELIMINAR ARTICULO DELETE
+app.delete('/api/articulos/:id', (req, res) => {
+   connection.query ("DELETE FROM articulos WHERE id = ?",[req.params.id],function (error, filas) {
+
+        if (error) {
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const puerto = process.env.PUERTO || 3000;
 
